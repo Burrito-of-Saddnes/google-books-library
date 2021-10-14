afa98e8675cc7bc680966a4aa4a75703d7a38bfa
Burrito-of-Saddnes <ersov8@gmail.com>
2021-10-14 16:48:29 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
+import { Provider } from 'mobx-react';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { RootStore } from './stores/RootStore';
+
+import './CSS/index.css';
+import App from './Components/App';
+
+declare global {
+  interface Window { rootState: any; }
+}
+
+const ROOT_STORE_INSTANCE = window.rootState = new RootStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider {...ROOT_STORE_INSTANCE}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
